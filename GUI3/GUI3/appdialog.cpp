03d9a42b78cc980e38637a1a88fdf829cd7f2a5c
Kbarrera123 <kaylabarrera@ufl.edu>
2018-04-21 15:52:20 -0400

    Added month choices to both dropdowns

--- GUI3/GUI3/appdialog.cpp
+++ GUI3/GUI3/appdialog.cpp
@@ -6,6 +6,32 @@ AppDialog::AppDialog(QWidget *parent) :
     ui(new Ui::AppDialog)
 {
     ui->setupUi(this);
+
+    ui->comboBox_2->addItem("January");
+    ui->comboBox_2->addItem("February");
+    ui->comboBox_2->addItem("March");
+    ui->comboBox_2->addItem("April");
+    ui->comboBox_2->addItem("May");
+    ui->comboBox_2->addItem("June");
+    ui->comboBox_2->addItem("July");
+    ui->comboBox_2->addItem("August");
+    ui->comboBox_2->addItem("September");
+    ui->comboBox_2->addItem("October");
+    ui->comboBox_2->addItem("November");
+    ui->comboBox_2->addItem("December");
+
+    ui->comboBox->addItem("January");
+    ui->comboBox->addItem("February");
+    ui->comboBox->addItem("March");
+    ui->comboBox->addItem("April");
+    ui->comboBox->addItem("May");
+    ui->comboBox->addItem("June");
+    ui->comboBox->addItem("July");
+    ui->comboBox->addItem("August");
+    ui->comboBox->addItem("September");
+    ui->comboBox->addItem("October");
+    ui->comboBox->addItem("November");
+    ui->comboBox->addItem("December");
 }
 
 AppDialog::~AppDialog()
