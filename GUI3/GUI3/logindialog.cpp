65beed1c782084c39a9d59e2237349a5f206bd9a
cruales <christie.ruales@gmail.com>
2018-04-21 17:05:21 -0400

    took out comments

--- GUI3/GUI3/logindialog.cpp
+++ GUI3/GUI3/logindialog.cpp
@@ -14,24 +14,6 @@ LogInDialog::~LogInDialog()
     delete ui;
 }
 
-/*
-void MainWindow::on_pushButton_Login_clicked()
-{
-    QString username = ui->lineEdit_username->text();
-    QString password = ui->lineEdit_password->text();
-
-       if(username ==  "test" && password == "test") {
-           QMessageBox::information(this, "Login", "Username and password is correct");
-           //hide();
-           appDialog = new AppDialog(this);
-           appDialog->show();
-       }
-       else {
-           QMessageBox::warning(this,"Login", "Username and password is not correct");
-       }
-}
-*/
-
 void LogInDialog::on_pushButton_Login_clicked()
 {
     QString username = ui->lineEdit_username->text();
